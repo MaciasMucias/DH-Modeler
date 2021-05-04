e94df6758d99ba7285ba700e7dcf0262936e8eac
MaciasMucias <maciej2.karaskiewicz@gmail.com>
2021-05-04 11:13:57 +0200

    VertexArray, VertexBufferLayout, VertexBufferLayout classes

--- Buffers.py
+++ Buffers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import ctypes
 from OpenGL.GL import *
 
 
@@ -14,6 +15,38 @@ class VertexBuffer:
     @staticmethod
     def unbind():
         glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+
+class VertexBufferLayout:
+    def __init__(self, count: int, gl_type: int, normalised: bool):
+        self.gl_type = gl_type
+        self.count = count
+        self.normalised = normalised
+
+
+gl_type_bytes = {
+        GL_FLOAT: ctypes.sizeof(ctypes.c_float),
+        GL_UNSIGNED_INT: ctypes.sizeof(ctypes.c_uint),
+        GL_UNSIGNED_BYTE: ctypes.sizeof(ctypes.c_byte)
+    }
+
+
+class VertexBufferLayout:
+    def __init__(self):
+        self.__elements = []
+        self.__stride = 0
+
+    def push(self, gl_type, count, normalised):
+        self.__elements.append(VertexLayoutElement(gl_type, count, normalised))
+        self.__stride += gl_type_bytes[gl_type]
+
+    @property
+    def stride(self):
+        return self.__stride
+
+    @property
+    def elements(self) -> [VertexLayoutElement]:
+        return self.__elements
 
 
 class IndexBuffer:
@@ -33,3 +66,26 @@ class IndexBuffer:
     @property
     def get_count(self):
         return self.__count
+
+
+class VertexArray:
+    def __init__(self):
+        self.__renderer_id = glGenVertexArrays(1)
+
+    def bind(self):
+        glBindVertexArray(self.__renderer_id)
+
+    @staticmethod
+    def unbind():
+        glBindVertexArray(0)
+
+    def add_buffer(self, vb: VertexBuffer, layout: VertexBufferLayout):
+        self.bind()
+        vb.bind()
+        elements = layout.elements
+        offset = 0
+        for ind, element in enumerate(elements):
+            glEnableVertexAttribArray(ind)
+            glVertexAttribPointer(ind, element.count, element.gl_type, element.normalised, layout.stride, offset)
+            offset += gl_type_bytes[element.gl_type] * element.count
+
