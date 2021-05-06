c68920c967ec715d7af40506e95f614591d937c9
MaciasMucias <maciej2.karaskiewicz@gmail.com>
2021-05-06 12:45:53 +0200

    Stopped using stretching for modifying cubes, as it was creating problems with rotation Added rotating around origin functionality to ObjectGroup

--- Objects3D.py
+++ Objects3D.py
@@ -14,19 +14,30 @@ class Object3D:
     def __init__(self, shape3d):
         self.model = glm.identity(glm.mat4)
         self.shape3d = shape3d
-        self.x = self.y = self.z = 1
+        self.x = self.y = self.z = 0
+        self.x_stretch = self.y_stretch = self.z_stretch = 1
 
-    def set_rotate(self, angle, axis):
-        self.model = glm.rotate(self.model, glm.radians(angle), glm.vec3(axis))
+    def rotate(self, angle, axis):
+        x, y, z = axis
+        self.model = glm.rotate(self.model, glm.radians(angle), glm.vec3(y, z, x))
 
-    def set_translate(self, x, y, z):
-        self.model = glm.translate(self.model, glm.vec3(x/self.x, y/self.y, z/self.z))
+    def rotate_around_origin(self, angle, axis):
+        x, y, z = self.x, self.y, self.z
+        self.translate(-x, -y, -z)
+        self.rotate(angle, axis)
+        self.translate(x, y, z)
+
+    def translate(self, x, y, z):
+        self.x += x
+        self.y += y
+        self.z += z
+        self.model = glm.translate(self.model, glm.vec3(-y/self.y_stretch, -z/self.z_stretch, x/self.x_stretch))
 
     def set_stretch(self, x, y, z):
-        self.x *= x
-        self.y *= y
-        self.z *= z
-        self.model = glm.scale(self.model, glm.vec3(x, y, z))
+        self.x_stretch *= x
+        self.y_stretch *= y
+        self.z_stretch *= z
+        self.model = glm.scale(self.model, glm.vec3(y, z, x))
 
     def draw(self, proj, view):
         self.shape3d.draw(proj, view, self.model)
@@ -49,48 +60,48 @@ class Shape3D:
 
 
 class Cube(Shape3D):
-    def __init__(self, material):
-        data = np.array([(-0.5, -0.5, -0.5, 0.0, 0.0, -1.0),
-                         ( 0.5, -0.5, -0.5, 0.0, 0.0, -1.0),
-                         ( 0.5,  0.5, -0.5, 0.0, 0.0, -1.0),
-                         ( 0.5,  0.5, -0.5, 0.0, 0.0, -1.0),
-                         (-0.5,  0.5, -0.5, 0.0, 0.0, -1.0),
-                         (-0.5, -0.5, -0.5, 0.0, 0.0, -1.0),
+    def __init__(self, material, x, y, z):
+        data = np.array([(-0.5*x, -0.5*y, -0.5*z, 0.0, 0.0, -1.0),
+                         ( 0.5*x, -0.5*y, -0.5*z, 0.0, 0.0, -1.0),
+                         ( 0.5*x,  0.5*y, -0.5*z, 0.0, 0.0, -1.0),
+                         ( 0.5*x,  0.5*y, -0.5*z, 0.0, 0.0, -1.0),
+                         (-0.5*x,  0.5*y, -0.5*z, 0.0, 0.0, -1.0),
+                         (-0.5*x, -0.5*y, -0.5*z, 0.0, 0.0, -1.0),
 
-                         (-0.5, -0.5,  0.5, 0.0, 0.0, 1.0),
-                         ( 0.5, -0.5,  0.5, 0.0, 0.0, 1.0),
-                         ( 0.5,  0.5,  0.5, 0.0, 0.0, 1.0),
-                         ( 0.5,  0.5,  0.5, 0.0, 0.0, 1.0),
-                         (-0.5,  0.5,  0.5, 0.0, 0.0, 1.0),
-                         (-0.5, -0.5,  0.5, 0.0, 0.0, 1.0),
+                         (-0.5*x, -0.5*y,  0.5*z, 0.0, 0.0, 1.0),
+                         ( 0.5*x, -0.5*y,  0.5*z, 0.0, 0.0, 1.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 0.0, 0.0, 1.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 0.0, 0.0, 1.0),
+                         (-0.5*x,  0.5*y,  0.5*z, 0.0, 0.0, 1.0),
+                         (-0.5*x, -0.5*y,  0.5*z, 0.0, 0.0, 1.0),
 
-                         (-0.5,  0.5,  0.5, -1.0, 0.0, 0.0),
-                         (-0.5,  0.5, -0.5, -1.0, 0.0, 0.0),
-                         (-0.5, -0.5, -0.5, -1.0, 0.0, 0.0),
-                         (-0.5, -0.5, -0.5, -1.0, 0.0, 0.0),
-                         (-0.5, -0.5,  0.5, -1.0, 0.0, 0.0),
-                         (-0.5,  0.5,  0.5, -1.0, 0.0, 0.0),
+                         (-0.5*x,  0.5*y,  0.5*z, -1.0, 0.0, 0.0),
+                         (-0.5*x,  0.5*y, -0.5*z, -1.0, 0.0, 0.0),
+                         (-0.5*x, -0.5*y, -0.5*z, -1.0, 0.0, 0.0),
+                         (-0.5*x, -0.5*y, -0.5*z, -1.0, 0.0, 0.0),
+                         (-0.5*x, -0.5*y,  0.5*z, -1.0, 0.0, 0.0),
+                         (-0.5*x,  0.5*y,  0.5*z, -1.0, 0.0, 0.0),
 
-                         ( 0.5,  0.5,  0.5, 1.0, 0.0, 0.0),
-                         ( 0.5,  0.5, -0.5, 1.0, 0.0, 0.0),
-                         ( 0.5, -0.5, -0.5, 1.0, 0.0, 0.0),
-                         ( 0.5, -0.5, -0.5, 1.0, 0.0, 0.0),
-                         ( 0.5, -0.5,  0.5, 1.0, 0.0, 0.0),
-                         ( 0.5,  0.5,  0.5, 1.0, 0.0, 0.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 1.0, 0.0, 0.0),
+                         ( 0.5*x,  0.5*y, -0.5*z, 1.0, 0.0, 0.0),
+                         ( 0.5*x, -0.5*y, -0.5*z, 1.0, 0.0, 0.0),
+                         ( 0.5*x, -0.5*y, -0.5*z, 1.0, 0.0, 0.0),
+                         ( 0.5*x, -0.5*y,  0.5*z, 1.0, 0.0, 0.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 1.0, 0.0, 0.0),
 
-                         (-0.5, -0.5, -0.5, 0.0, -1.0, 0.0),
-                         ( 0.5, -0.5, -0.5, 0.0, -1.0, 0.0),
-                         ( 0.5, -0.5,  0.5, 0.0, -1.0, 0.0),
-                         ( 0.5, -0.5,  0.5, 0.0, -1.0, 0.0),
-                         (-0.5, -0.5,  0.5, 0.0, -1.0, 0.0),
-                         (-0.5, -0.5, -0.5, 0.0, -1.0, 0.0),
+                         (-0.5*x, -0.5*y, -0.5*z, 0.0, -1.0, 0.0),
+                         ( 0.5*x, -0.5*y, -0.5*z, 0.0, -1.0, 0.0),
+                         ( 0.5*x, -0.5*y,  0.5*z, 0.0, -1.0, 0.0),
+                         ( 0.5*x, -0.5*y,  0.5*z, 0.0, -1.0, 0.0),
+                         (-0.5*x, -0.5*y,  0.5*z, 0.0, -1.0, 0.0),
+                         (-0.5*x, -0.5*y, -0.5*z, 0.0, -1.0, 0.0),
 
-                         (-0.5,  0.5, -0.5, 0.0, 1.0, 0.0),
-                         ( 0.5,  0.5, -0.5, 0.0, 1.0, 0.0),
-                         ( 0.5,  0.5,  0.5, 0.0, 1.0, 0.0),
-                         ( 0.5,  0.5,  0.5, 0.0, 1.0, 0.0),
-                         (-0.5,  0.5,  0.5, 0.0, 1.0, 0.0),
-                         (-0.5,  0.5, -0.5, 0.0, 1.0, 0.0)], dtype=np.float32)
+                         (-0.5*x,  0.5*y, -0.5*z, 0.0, 1.0, 0.0),
+                         ( 0.5*x,  0.5*y, -0.5*z, 0.0, 1.0, 0.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 0.0, 1.0, 0.0),
+                         ( 0.5*x,  0.5*y,  0.5*z, 0.0, 1.0, 0.0),
+                         (-0.5*x,  0.5*y,  0.5*z, 0.0, 1.0, 0.0),
+                         (-0.5*x,  0.5*y, -0.5*z, 0.0, 1.0, 0.0)], dtype=np.float32)
 
         layout = VertexBufferLayout()
         layout.push(GL_FLOAT, 3, GL_FALSE, "position")
@@ -114,13 +125,13 @@ class ObjectGroup:
     def add_object(self, object3d):
         self.objects.append(object3d)
 
-    def set_rotate(self, angle, axis):
+    def rotate(self, angle, axis):
         for i in self.objects:
-            i.set_rotate(angle, axis)
+            i.rotate_around_origin(angle, axis)
 
-    def set_translate(self, x, y, z):
+    def translate(self, x, y, z):
         for i in self.objects:
-            i.set_translate(x, y, z)
+            i.translate(x, y, z)
 
     def draw(self, proj, view):
         for i in self.objects:
@@ -140,24 +151,21 @@ material_red.add_uniform(material_red.shader.set_uniform_3f, 'uColor', (1, 0, 0)
 material_green.add_uniform(material_green.shader.set_uniform_3f, 'uColor', (0, 1, 0))
 material_blue.add_uniform(material_blue.shader.set_uniform_3f, 'uColor', (0, 0, 1))
 
-gray_cube = Cube(material_gray)
-red_cube = Cube(material_red)
-green_cube = Cube(material_green)
-blue_cube = Cube(material_blue)
 
 rod = 0.2
+gray_cube = Cube(material_gray, 0.5, 0.5, 0.5)
+red_cube = Cube(material_red, rod, rod, 1)
+green_cube = Cube(material_green, 1, rod, rod)
+blue_cube = Cube(material_blue, rod, 1, rod)
+
 
 cube1 = Object3D(gray_cube)
 cube2 = Object3D(red_cube)
 cube3 = Object3D(green_cube)
 cube4 = Object3D(blue_cube)
-cube1.set_stretch(0.5, 0.5, 0.5)
-cube2.set_stretch(1, rod, rod)
-cube2.set_translate(0.5, 0, 0)
-cube3.set_stretch(rod, 1, rod)
-cube3.set_translate(0, 0.5, 0)
-cube4.set_stretch(rod, rod, 1)
-cube4.set_translate(0, 0, 0.5)
+cube2.translate(0.5, 0, 0)
+cube3.translate(0, 0.5, 0)
+cube4.translate(0, 0, 0.5)
 
 coord_3d = ObjectGroup()
 coord_3d.add_object(cube1)
