b905c1b61ce47126bc515d28581a02c4f46e2c2e
MaciasMucias <maciej2.karaskiewicz@gmail.com>
2021-05-07 18:47:49 +0200

    Added simple camera controls

--- GLWidget.py
+++ GLWidget.py
@@ -1,6 +1,7 @@
+import numpy as np
 from OpenGL.GL import *
 import glm
-from PyQt5 import QtWidgets
+from PyQt5 import QtGui, QtWidgets
 
 from Renderer import Renderer
 from Matrices import mat4, ViewMat
@@ -11,6 +12,11 @@ class GLWidget(QtWidgets.QOpenGLWidget):
         super(GLWidget, self).__init__(parent)
         self.Robot = Robot
         self.setFormat(glFormat)
+
+        self.mouse_down = False
+        self.x = self.y = None
+        self.rh = 0
+        self.rv = 30
 
     def initializeGL(self) -> None:
         self.Robot.initialise()
@@ -29,3 +35,25 @@ class GLWidget(QtWidgets.QOpenGLWidget):
     def paintGL(self):
         Renderer.clear()
         self.Robot.draw(self.proj.mat, self.view.mat)
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.mouse_down = True
+        self.x, self.y = a0.x(), a0.y()
+
+    def mouseReleaseEvent(self, a0: QtGui.QMouseEvent) -> None:
+        self.mouse_down = False
+
+    def mouseMoveEvent(self, a0: QtGui.QMouseEvent) -> None:
+        if self.mouse_down:
+            x, y = a0.x(), a0.y()
+            dx, dy = (x - self.x) * 0.25, (y - self.y)*0.25
+            self.x, self.y = a0.x(), a0.y()
+            if self.rv + dy < -90:
+                dy = -90 - self.rv
+            elif self.rv + dy > 90:
+                dy = 90 - self.rv
+            self.view.rotate(dy, (np.sin(np.radians(self.rh)), np.cos(np.radians(self.rh)), 0))
+            self.rv += dy
+            self.view.rotate(dx, (0, 0, 1))
+            self.rh += dx
+            self.update()
